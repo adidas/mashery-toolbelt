aa51e545872aa77d06faa45794efd38c5822ec80
David Schovanec <schovanec@schovi.cz>
2018-03-26 18:25:45 +0200

    Lint missed root files

--- src/client.js
+++ src/client.js
@@ -13,18 +13,18 @@ let credentials
 try {
   const credentialsFile = readFileSync(CREDENTIALS_FILE)
   credentials = JSON.parse(credentialsFile)
-} catch(err) {
-  if(err.code !== 'ENOENT') {
+} catch (err) {
+  if (err.code !== 'ENOENT') {
     throw err
   }
 }
 
 module.exports = new MasheryClient({
   credentials,
-  onAuthenticationSuccess: (credentials) => {
-    writeFileSync(CREDENTIALS_FILE, JSON.stringify(credentials, null , 2))
+  onAuthenticationSuccess: credentials => {
+    writeFileSync(CREDENTIALS_FILE, JSON.stringify(credentials, null, 2))
   },
-  onAuthenticationError: (error) => {
+  onAuthenticationError: error => {
     console.error(`${error.name}: ${error.message}`)
     console.log('Try running `heroku-toolbelt auth` again')
     process.exit(1)
