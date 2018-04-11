c72766e4c2b3c8a5ae042b9b3383079c03f3eda9
David Schovanec <schovanec@schovi.cz>
2018-04-11 11:45:01 +0200

    Package blueprint prop types

---
+++ src/workflow/adidas/utils/packageBlueprintPropTypes.js
@@ -0,0 +1,77 @@
+const PropTypes = require('prop-types')
+
+const {
+  arrayOf,
+  bool,
+  number,
+  string,
+  object,
+  oneOf,
+  oneOfType,
+  shape
+} = PropTypes
+
+// Shortcut for single value
+const value = value => oneOf([value])
+const timeIntervals = oneOf(['minute', 'hour', 'day', 'week', 'month'])
+const allOrArrayOf = shape => oneOfType([value('all'), arrayOf(shape)])
+const methods = allOrArrayOf(shape({ name: string }))
+
+// Main definition
+module.exports = {
+  package: shape({
+    name: string.isRequired,
+    description: string,
+    eav: object,
+    keyLength: number,
+    nearQuotaThreshold: number,
+    notifyAdminEmails: string,
+    notifyAdminPeriod: timeIntervals,
+    notifyDeveloperPeriod: timeIntervals,
+    sharedSecretLength: number,
+    notifyAdminNearQuota: bool,
+    notifyAdminOverQuota: bool,
+    notifyAdminOverThrottle: bool,
+    notifyDeveloperNearQuota: bool,
+    notifyDeveloperOverQuota: bool,
+    notifyDeveloperOverThrottle: bool
+  }),
+  plans: arrayOf(
+    shape({
+      name: string.isRequired,
+      description: string,
+      status: oneOf(['active', 'inactive']),
+      emailTemplateSetId: string,
+      eav: object,
+      notes: string,
+      maxNumKeysAllowed: number,
+      numKeysBeforeReview: number,
+      qpsLimitCeiling: number,
+      rateLimitCeiling: number,
+      rateLimitPeriod: timeIntervals,
+      adminKeyProvisioningEnabled: bool,
+      qpsLimitExempt: bool,
+      qpsLimitKeyOverrideAllowed: bool,
+      rateLimitExempt: bool,
+      rateLimitKeyOverrideAllowed: bool,
+      responseFilterOverrideAllowed: bool,
+      selfServiceKeyProvisioningEnabled: bool,
+
+      services: arrayOf(
+        shape({
+          id: string,
+          name: string,
+          endpoints: allOrArrayOf(
+            shape({
+              id: string,
+              name: string,
+              undefinedMethodsAllowed: bool,
+              methods
+            })
+          ).isRequired,
+          methods
+        })
+      )
+    })
+  )
+}
