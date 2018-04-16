fe003c4dad1556d8c79ff51df1788f271d216e9d
David Schovanec <schovanec@schovi.cz>
2018-04-16 23:07:12 +0200

    Refactor and optimize throttling

--- src/lib/client/throttle.js
+++ src/lib/client/throttle.js
@@ -1,54 +1,139 @@
-function throttle (client, originalCall) {
-  if (process.env['DISABLE_THROTTLE']) {
-    return originalCall()
+/**
+ * Pool data are stored locally
+ */
+const POOLS = new Map()
+let WORKER_COUNTER = 0
+
+/**
+ * Representation of pool with workers and pending calls
+ *
+ * @class Pool
+ */
+class Pool {
+  constructor () {
+    this.calls = []
+    this.workers = []
   }
 
-  // Initialize pool for parallels calls
-  if (!client.__pool) {
-    client.__pool = [...Array(client.options.threads)].map((_, i) => ({
-      id: i
-    }))
-    client.__pendingCalls = []
+  updateWorkers (threads) {
+    const currentWorkers = this.workers.length
+    if (threads > currentWorkers) {
+      for (let i = currentWorkers + 1; i <= threads; i++) {
+        this.workers.push(new Worker(++WORKER_COUNTER))
+      }
+    } else if (threads < currentWorkers) {
+      this.workers.splice(threads).forEach(worker => (worker.running = false))
+    }
+  }
+}
+
+/**
+ * Representation of worker
+ *
+ * @class Worker
+ */
+class Worker {
+  constructor (id) {
+    this.id = id
+    this.running = false
+  }
+}
+
+/**
+ * Throttle parallel calls to API
+ *
+ * @param {any} key - identifier of pool
+ * @param {function(): any} originalCall
+ * @returns {Promise<any, Error>}
+ */
+function throttle (key, call, threads = 2) {
+  if (threads <= 0 || process.env['DISABLE_THROTTLE']) {
+    try {
+      return Promise.resolve(call())
+    } catch (e) {
+      Promise.reject(e)
+    }
   }
 
-  let callResolve
-  let callReject
+  const pool = initializePool(key, threads)
+  return throttleCall(pool, call)
+}
 
-  const promise = new Promise((resolve, reject) => {
-    callResolve = resolve
-    callReject = reject
+/**
+ * Throttle parallel calls to API based on client.options.threads size
+ *
+ * @param {any} key - identifier of pool
+ * @returns {Object} pool
+ * @property {Array<function(): Promise<any, Error>>} pool.calls
+ * @property {Array<{id: number, running: boolean}>} pool.workers
+ */
+function initializePool (key, threads) {
+  // Initialize pool
+  let pool = POOLS.get(key)
+
+  if (!pool) {
+    pool = new Pool()
+    POOLS.set(key, pool)
+  }
+
+  pool.updateWorkers(threads)
+
+  return pool
+}
+
+/**
+ * Throttle parallel calls to API based on client.options.threads size
+ *
+ * @param {Pool} pool
+ * @param {function(): Promise<any, Error>} originalCall
+ * @returns {Promise<Response, Error>}
+ */
+function throttleCall (pool, call) {
+  return new Promise((resolve, reject) => {
+    const pendingCall = () => {
+      // Assume that original call can be async call, or function with plain result
+      try {
+        return Promise.resolve(call()).then(resolve, reject)
+      } catch (e) {
+        reject(e)
+      }
+    }
+
+    pool.calls.push(pendingCall)
+    runPool(pool)
   })
-
-  const call = () => {
-    return originalCall()
-      .then(result => callResolve(result))
-      .catch(error => callReject(error))
-  }
-
-  client.__pendingCalls.push(call)
-
-  run(client)
-
-  return promise
 }
 
-function processPendingCalls (client, runner) {
-  const pendingCall = client.__pendingCalls.shift()
-
-  if (pendingCall) {
-    pendingCall().then(() => processPendingCalls(client, runner))
-  } else {
-    runner.running = false
-  }
-}
-
-function run (client) {
-  client.__pool.forEach(runner => {
-    if (!runner.running) {
-      runner.running = true
-      processPendingCalls(client, runner)
+/**
+ * Starts pool
+ *
+ * @param {Pool} client
+ */
+function runPool (pool) {
+  pool.workers.forEach(worker => {
+    if (!worker.running) {
+      worker.running = true
+      processCallInWorker(pool, worker)
     }
   })
+}
+
+/**
+ * Get call from pool and run it within worker
+ *
+ * @param {Pool} pool
+ * @param {Worker} worker
+ */
+function processCallInWorker (pool, worker) {
+  if (!worker.running) return
+
+  const call = pool.calls.shift()
+
+  if (call) {
+    call().then(() => processCallInWorker(pool, worker))
+  } else {
+    worker.running = false
+  }
 }
 
 module.exports = throttle
